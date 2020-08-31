ba9fcf6c08e2bd55526095cccc539902a65d7442
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-08-31 16:26:24 -0500

    Primero

---
+++ README.md
@@ -0,0 +1,3 @@
+Actualiza ejemplo de https://medium.com/dailyjs/building-a-react-component-with-webpack-publish-to-npm-deploy-to-github-guide-6927f60b3220
+
+Para iniciar ejemplo
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './styles.css'
+
+export default class CompReactWebpackStiff2018 extends React.Component {
+  render () {
+      return (<h1>Hello from My Component</h1>)
+  }
+}
+
